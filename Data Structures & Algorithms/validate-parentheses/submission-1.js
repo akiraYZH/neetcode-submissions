fd35eaf8cc1540120af03ff86224f5d3d6b87d8f
akiraYZH <51339684+akiraYZH@users.noreply.github.com>
2026-07-08 23:56:52 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const map = {
+            "]":"[",
+            ")":"(",
+            "}":"{"
+        }
+
+        for(let i = 0; i < s.length; i++){
+            if(!map[s[i]]){
+                stack.push(s[i])
+            }else if(map[s[i]] === stack[stack.length-1]){
+                stack.pop();
+            }else{
+                return false
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
