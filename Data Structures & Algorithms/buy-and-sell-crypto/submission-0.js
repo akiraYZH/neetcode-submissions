a91d3fe211de5a1e7baa4a6979363fea9605399b
akiraYZH <51339684+akiraYZH@users.noreply.github.com>
2026-07-19 23:11:51 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0;
+        let r = 1;
+        let maxProfit = 0;
+
+        while(r < prices.length){
+            if(prices[l] < prices[r]){
+                maxProfit = Math.max(maxProfit, prices[r] - prices[l]);
+            }else{
+                l = r;
+            }
+
+            r++;
+        }
+
+        return maxProfit
+    }
+}
