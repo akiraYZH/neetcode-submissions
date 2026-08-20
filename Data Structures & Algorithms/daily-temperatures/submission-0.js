b60f07083740a70ef56cd68cab46e5460f054c44
akiraYZH <51339684+akiraYZH@users.noreply.github.com>
2026-08-20 09:43:38 -0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const res = new Array(temperatures.length).fill(0);
+        const stack = [];
+
+        for(let i = 0; i < temperatures.length; i++){
+            const t = temperatures[i];
+
+            while(stack.length && t > stack[stack.length - 1][0]){
+                const [temp, index] = stack.pop();
+                res[index] = i - index;
+            }
+
+            stack.push([t, i]);
+        }
+
+        return res;
+    }
+}
