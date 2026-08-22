a20b9517e6a358bc96565d736034c91af66cbff6
akiraYZH <51339684+akiraYZH@users.noreply.github.com>
2026-08-22 13:44:00 -0700

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head) {
+         if (!head) return;
+
+        let curr = head;
+        let arr = [];
+        while(curr){
+            arr.push(curr);
+            curr = curr.next;
+        }
+
+        let l = 0;
+        let r = arr.length -1;
+
+        while(l < r){
+            arr[l].next = arr[r];
+            l++;
+
+            if(l >= r) break;
+
+            arr[r].next = arr[l];
+            r--;
+        }
+
+        arr[l].next = null;
+
+        return head
+    }
+}
