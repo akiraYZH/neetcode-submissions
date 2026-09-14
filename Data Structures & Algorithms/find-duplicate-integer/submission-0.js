18fcc60e9eb02caaba088b1414c716e7cd643963
akiraYZH <51339684+akiraYZH@users.noreply.github.com>
2026-09-14 10:26:49 -0700

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        const set = new Set();
+
+        for(let i = 0; i < nums.length; i++){
+            if(!set.has(nums[i])){
+                set.add(nums[i])
+            }else{
+                return nums[i]
+            }
+        }
+    }
+}
